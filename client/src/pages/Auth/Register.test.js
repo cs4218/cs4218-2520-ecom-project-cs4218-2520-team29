479e902f8b433cd555cdd7984c1b125f97c8ab2d
jiayr99 <e1249294@u.nus.edu>
2026-02-23 01:00:44 +0800

    feat: add more test case to login and register

--- client/src/pages/Auth/Register.test.js
+++ client/src/pages/Auth/Register.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import axios from 'axios';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
@@ -11,33 +11,33 @@ jest.mock('axios');
 jest.mock('react-hot-toast');
 
 jest.mock('../../context/auth', () => ({
-    useAuth: jest.fn(() => [null, jest.fn()]) // Mock useAuth hook to return null state and a mock function for setAuth
-  }));
+  useAuth: jest.fn(() => [null, jest.fn()]) // Mock useAuth hook to return null state and a mock function for setAuth
+}));
 
-  jest.mock('../../context/cart', () => ({
-    useCart: jest.fn(() => [null, jest.fn()]) // Mock useCart hook to return null state and a mock function
-  }));
+jest.mock('../../context/cart', () => ({
+  useCart: jest.fn(() => [null, jest.fn()]) // Mock useCart hook to return null state and a mock function
+}));
     
 jest.mock('../../context/search', () => ({
-    useSearch: jest.fn(() => [{ keyword: '' }, jest.fn()]) // Mock useSearch hook to return null state and a mock function
-  }));  
+  useSearch: jest.fn(() => [{ keyword: '' }, jest.fn()]) // Mock useSearch hook to return null state and a mock function
+}));  
 
-  Object.defineProperty(window, 'localStorage', {
-    value: {
-      setItem: jest.fn(),
-      getItem: jest.fn(),
-      removeItem: jest.fn(),
-    },
-    writable: true,
-  });
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    setItem: jest.fn(),
+    getItem: jest.fn(),
+    removeItem: jest.fn(),
+  },
+  writable: true,
+});
 
 window.matchMedia = window.matchMedia || function() {
-    return {
-      matches: false,
-      addListener: function() {},
-      removeListener: function() {}
-    };
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
   };
+};
       
 
 describe('Register Component', () => {
@@ -45,37 +45,114 @@ describe('Register Component', () => {
     jest.clearAllMocks();
   });
 
+  // Chia Jia Ye A0286580U
+  it('should render register form correctly', () => {
+    render(
+      <MemoryRouter initialEntries={['/register']}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('REGISTER FORM')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter Your Name')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter Your Email')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter Your Password')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter Your Phone')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter Your Address')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter Your DOB')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('What is Your Favorite Sport')).toBeInTheDocument();
+  });
+
+  // Chia Jia Ye A0286580U
+  it('should initialise inputs as empty', () => {
+    render(
+      <MemoryRouter initialEntries={['/register']}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByPlaceholderText('Enter Your Name').value).toBe('');
+    expect(screen.getByPlaceholderText('Enter Your Email').value).toBe('');
+    expect(screen.getByPlaceholderText('Enter Your Password').value).toBe('');
+    expect(screen.getByPlaceholderText('Enter Your Phone').value).toBe('');
+    expect(screen.getByPlaceholderText('Enter Your Address').value).toBe('');
+    expect(screen.getByPlaceholderText('Enter Your DOB').value).toBe('');
+    expect(screen.getByPlaceholderText('What is Your Favorite Sport').value).toBe('');
+  });
+
   it('should register the user successfully', async () => {
     axios.post.mockResolvedValueOnce({ data: { success: true } });
     axios.get.mockResolvedValueOnce({ data: { category: [] } });
 
-    const { getByText, getByPlaceholderText } = render(
-        <MemoryRouter initialEntries={['/register']}>
-          <Routes>
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </MemoryRouter>
-      );
+    render(
+      <MemoryRouter initialEntries={['/register']}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </MemoryRouter>
+    );
 
-    fireEvent.change(getByPlaceholderText('Enter Your Name'), { target: { value: 'John Doe' } });
-    fireEvent.change(getByPlaceholderText('Enter Your Email'), { target: { value: 'test@example.com' } });
-    fireEvent.change(getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
-    fireEvent.change(getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
-    fireEvent.change(getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
-    fireEvent.change(getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
-    fireEvent.change(getByPlaceholderText('What is Your Favorite sports'), { target: { value: 'Football' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Name'), { target: { value: 'John Doe' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Email'), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
+    fireEvent.change(screen.getByPlaceholderText('What is Your Favorite Sport'), { target: { value: 'Football' } });
 
-    fireEvent.click(getByText('REGISTER'));
+    fireEvent.click(screen.getByText('REGISTER'));
 
     await waitFor(() => expect(axios.post).toHaveBeenCalled());
     expect(toast.success).toHaveBeenCalledWith('Register Successfully, please login');
+  });
+
+  // Chia Jia Ye A0286580U
+  it('should send correct payload to backend', async () => {
+    axios.post.mockResolvedValueOnce({ data: { success: true } });
+
+    render(
+      <MemoryRouter initialEntries={['/register']}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Name'), { target: { value: 'John Doe' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Email'), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
+    fireEvent.change(screen.getByPlaceholderText('What is Your Favorite Sport'), { target: { value: 'Football' } });
+
+    fireEvent.click(screen.getByText('REGISTER'));
+
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledWith(
+        '/api/v1/auth/register',
+        {
+          name: 'John Doe',
+          email: 'test@example.com',
+          password: 'password123',
+          phone: '1234567890',
+          address: '123 Street',
+          DOB: '2000-01-01',
+          answer: 'Football'
+        }
+      );
+    });
   });
 
   it('should display error message on failed registration', async () => {
     axios.post.mockRejectedValueOnce({ message: 'User already exists' });
     axios.get.mockResolvedValueOnce({ data: { category: [] } });
 
-    const { getByText, getByPlaceholderText } = render(
+    render(
         <MemoryRouter initialEntries={['/register']}>
           <Routes>
             <Route path="/register" element={<Register />} />
@@ -83,17 +160,49 @@ describe('Register Component', () => {
         </MemoryRouter>
       );
 
-    fireEvent.change(getByPlaceholderText('Enter Your Name'), { target: { value: 'John Doe' } });
-    fireEvent.change(getByPlaceholderText('Enter Your Email'), { target: { value: 'test@example.com' } });
-    fireEvent.change(getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
-    fireEvent.change(getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
-    fireEvent.change(getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
-    fireEvent.change(getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
-    fireEvent.change(getByPlaceholderText('What is Your Favorite sports'), { target: { value: 'Football' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Name'), { target: { value: 'John Doe' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Email'), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
+    fireEvent.change(screen.getByPlaceholderText('What is Your Favorite Sport'), { target: { value: 'Football' } });
 
-    fireEvent.click(getByText('REGISTER'));
+    fireEvent.click(screen.getByText('REGISTER'));
 
     await waitFor(() => expect(axios.post).toHaveBeenCalled());
     expect(toast.error).toHaveBeenCalledWith('Something went wrong');
+  });
+
+  // Chia Jia Ye A0286580U
+  it('should display error toast when registration success is false', async () => {
+    axios.post.mockResolvedValueOnce({
+      data: {
+        success: false,
+        message: 'Email already exists'
+      }
+    });
+
+    render(
+      <MemoryRouter initialEntries={['/register']}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Name'), { target: { value: 'John Doe' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Email'), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
+    fireEvent.change(screen.getByPlaceholderText('What is Your Favorite Sport'), { target: { value: 'Football' } });
+
+    fireEvent.click(screen.getByText('REGISTER'));
+
+    await waitFor(() => {
+      expect(toast.error).toHaveBeenCalledWith('Email already exists');
+    });
   });
 });
